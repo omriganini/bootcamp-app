2c28f94e6cd8e802ae413c2244fd75232a4d1807
omriganini <omriganini6@gmail.com>
2022-06-11 10:01:25 +0000

    azure week

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const init = async () => {
 	dotenv.config();
 	const config = {
 		port: process.env.PORT || 8080,
-		host: process.env.HOST || "34.147.96.122"
+		host: process.env.HOST || "20.79.162.220"
 	};
 	const server = await createServer( config );
 	await server.start();
